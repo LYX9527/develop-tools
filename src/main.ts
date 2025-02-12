f5a1924f882571d1b2cdaf536683a21682cdaf8d
setruth <1607908758@qq.com>
2025-02-12 11:16:47 +0800

    引入全局样式

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-
+import './style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-// import router from './router'
 
 const app = createApp(App)
 
