7c6f13255b21fecfaf0a72fb5e85959ee819151d
LYX <lyxing9527@163.com>
2025-02-14 10:03:41 +0800

    feat: 新增命名转换，配置自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+        AutoImport({
+            resolvers: [NaiveUiResolver()],
+            imports: ["vue", "vue-router"],
+            dts: 'types/auto-imports.d.ts'
+        }),
+        Components({
+            resolvers: [NaiveUiResolver()],
+            dts: 'types/components.d.ts',
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
+    server: {
+        hmr: {
+            overlay: false
+        }
+    }
 })
