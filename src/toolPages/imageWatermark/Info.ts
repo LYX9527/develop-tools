7f8b13345ece53bfe08d8d17078d407d952d5d62
setruth <1607908758@qq.com>
2025-02-18 21:21:23 +0800

    feat: 新增图片水印工具的Icon

--- src/toolPages/imageWatermark/Info.ts
+++ src/toolPages/imageWatermark/Info.ts
@@ -1,8 +1,9 @@
 import type {ToolBaseInfo} from "@/models/ToolInfo.ts";
+import Icon from './Icon.vue';
 const toolInfo:ToolBaseInfo = {
     name: '图片水印',
     description: '此工具可以对图片添加自定义的水印文字，并且自定义水印的相关的布局和方向',
-    icon:null,
+    icon:Icon,
     tags:['图片','水印',"自定义"],
     author:'Setruth',
 }
