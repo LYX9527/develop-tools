6ed655364fc646441bb820089b4bd518bd6d8466
DuoBaoWa <liqiziqian@gmail.com>
2025-02-18 15:45:18 +0800

    add MD5 github info
    add MD5 github info

--- src/toolPages/md5/Info.ts
+++ src/toolPages/md5/Info.ts
@@ -6,7 +6,8 @@ const toolInfo: ToolBaseInfo = {
     name: "MD5加密",
     description: "将输入文本转换为MD5加密格式，支持32位和16位大小写输出",
     tags: ["加密", "MD5"],
-    author: "duobaowa"
+    author: "duobaowa",
+    github: "https://github.com/DuoBaoWa"
 }
 export default toolInfo;
 
