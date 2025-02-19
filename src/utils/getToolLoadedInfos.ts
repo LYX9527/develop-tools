0a70d78ffa5b16da821417405f2b85ac4fdfff67
setruth <1607908758@qq.com>
2025-02-19 17:52:53 +0800

    chore: 对工具信息列表初始化更新了闭包功能，避免同时getList和getBy访问的时候出现的重复调用加载的情况，也避免一些并发调用的奇怪问题

--- src/utils/getToolLoadedInfos.ts
+++ src/utils/getToolLoadedInfos.ts
@@ -19,15 +19,42 @@ export const getToolLoadedInfoByToolTag = async (toolTag: string): Promise<ToolL
     throw new Error(`找不到${toolTag}`);
 };
 
-
-async function initToolLoadedInfos() {
-    const files = import.meta.glob('@/toolPages/*/Info.ts');
-    toolInfosCache = [];
-    const promises = Object.entries(files).map(async ([filePath, moduleLoader]) => {
-        const module = await moduleLoader() as { default: ToolBaseInfo };
-        const toolTag = filePath.split('toolPages/').pop()?.split('/Info.ts')[0];
-        if (!toolTag) return
-        toolInfosCache!!.push({...module.default, toolTag})
-    });
-    await Promise.all(promises);
-}
+//闭包避免并发调用初始化重复给缓存赋值
+const initToolLoadedInfos = (() => {
+    let promise: Promise<void> | null = null;
+    return async () => {
+        if (promise) {
+            return promise;
+        }
+        return promise = new Promise<void>(async (resolve,_) => {
+            try {
+                const files = import.meta.glob('@/toolPages/*/Info.ts');
+                const toolInfosCacheTemp: ToolLoadedInfo[] = [];
+                const promises = Object.entries(files).map(async ([filePath, moduleLoader]) => {
+                    const module = await moduleLoader() as { default: ToolBaseInfo };
+                    const toolTag = filePath.split('toolPages/').pop()?.split('/Info.ts')[0];
+                    if (!toolTag) return;
+                    toolInfosCacheTemp.push({...module.default, toolTag});
+                });
+                await Promise.all(promises);
+                toolInfosCache = toolInfosCacheTemp;
+                resolve()
+            } catch (error) {
+                console.error("初始化工具列表失败", error)
+            } finally {
+                promise = null;
+            }
+        });
+    };
+})();
+// async function initToolLoadedInfos() {
+//     const files = import.meta.glob('@/toolPages/*/Info.ts');
+//     toolInfosCache = [];
+//     const promises = Object.entries(files).map(async ([filePath, moduleLoader]) => {
+//         const module = await moduleLoader() as { default: ToolBaseInfo };
+//         const toolTag = filePath.split('toolPages/').pop()?.split('/Info.ts')[0];
+//         if (!toolTag) return
+//         toolInfosCache!!.push({...module.default, toolTag})
+//     });
+//     await Promise.all(promises);
+// }
