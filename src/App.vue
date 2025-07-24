366f99f734b824161c4ab9e29a9073426d301f4d
LYX <lyxing9527@163.com>
2025-07-24 17:34:41 +0800

    Initialize project with Vue 3, TypeScript, TailwindCSS, and Vite configuration. Include basic setup for file structure, `.gitignore`, and essential dependencies.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-gray-100 p-4 h-screen flex items-center justify-center">
+    Hello
+    <span class="text-red-700">World!</span>
+  </div>
+
+
+</template>
+
+<style scoped>
+</style>
