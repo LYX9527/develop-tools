a3e003e786cb05fe4a9737dddb8d26f8343e8c79
LYX <lyxing9527@163.com>
2025-07-24 17:39:24 +0800

    feat: Add new dependencies and plugins, configure Vite, and include environment files.
    - Added `@types/node`, `pinia`, `vue-router`, `unplugin-auto-import`, and `unplugin-vue-components`.
    - Introduced MIT License.
    - Configured Vite for path aliases and auto-imports.
    - Added development and production environment variable files.
    - Updated `.gitignore` for lock file exclusion.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), tailwindcss()],
+    plugins: [
+        vue(),
+        AutoImport({
+            imports: ["vue", "vue-router"],
+            dts: 'types/auto-imports.d.ts'
+        }),
+        Components({
+            dts: 'types/components.d.ts',
+        }),
+        tailwindcss()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
 })
